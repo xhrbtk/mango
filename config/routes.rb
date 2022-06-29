7333700b7cc96cdfb85d9de33a2c612df4df2caf
xhrbtk <xhtbtk@gmail.com>
2022-06-29 13:17:13 +0800

    feat:create model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show'
 end
